b40a72c0c49c1a70879fade733685474347cd7d7
jenniferzrd <jennifer.zarid@gmail.com>
2018-10-05 16:40:01 +0200

    delete exercise

--- foreign_key/SpringDataCompositePrimaryKey/src/main/java/com/javasampleapproach/compositeprimarykey/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.javasampleapproach.compositeprimarykey.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.javasampleapproach.compositeprimarykey.model.OrderDetail;
-
-public interface OrderRepository extends JpaRepository<OrderDetail, Integer>{
-}
